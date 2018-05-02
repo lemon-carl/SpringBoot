30a7788fbb8f6ed8716f5218e7085fa76043994c
MrLing <LingHuan5593@qq.com>
2018-05-02 22:16:26 +0800

    SpringBoot project girl Controller的使用 submit

--- girl/src/main/java/com/hr/girl/HelloController.java
+++ girl/src/main/java/com/hr/girl/HelloController.java
@@ -8,9 +8,8 @@ package com.hr.girl;/**
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @Name : HelloController
@@ -19,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description : 
  */
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
 
 /*    @Value("${cupSize}")
@@ -33,8 +33,11 @@ public class HelloController {
     @Autowired
     private GirlProperties girlProperties;
 
-    @RequestMapping(value = "/hello",method = RequestMethod.GET)
-    public String say(){
-        return girlProperties.getCupSize();
+   // @RequestMapping(value = "/say",method = RequestMethod.GET)
+    @GetMapping(value="/say")
+    public String say(@RequestParam(value="id",required = false,defaultValue = "0") Integer myid){
+      return  "id:" + myid;
+      //  return girlProperties.getCupSize();
+     // return "index";
     }
 }
