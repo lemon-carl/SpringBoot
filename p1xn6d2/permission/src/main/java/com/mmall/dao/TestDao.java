4fdb589d45f9975ab425f8b3bc7ddb32f0c37435
MrLing <LingHuan5593@qq.com>
2019-01-12 11:54:54 +0800

    生成核心类，接口，xml文件

---
+++ p1xn6d2/permission/src/main/java/com/mmall/dao/TestDao.java
@@ -0,0 +1,11 @@
+package com.mmall.dao;
+
+/**
+ * @InterFaceName : TestDao
+ * Created with IDEA
+ * @author:CarlLing
+ * @CreateDate : 2019-01-10 0:19
+ * @Description :
+ */
+public interface TestDao {
+}
