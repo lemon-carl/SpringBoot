a7284305e708dab98156938a2c9193afe0dea3b6
CarlLemon <LingHuan5593@qq.com>
2021-04-08 01:18:04 +0800

    JWT Token 工具类

---
+++ yeb/yeb-server/src/main/java/com/lemon/server/config/security/JwtTokenUtil.java
@@ -0,0 +1,147 @@
+package com.lemon.server.config.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Date : 2021/4/8 0:40
+ * @author: CarlLing
+ * @Version 1.0
+ * @Desc :    JwtToken 工具类
+ */
+public class JwtTokenUtil {
+
+    public static final String CLAIM_KEY_USERNAME = "sub";
+    public static final String CLAIM_KEY_CREATED = "created";
+
+    @Value("${jwt.secret}")
+    private String secret;
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+    /**
+     * 根据用户信息生成 token
+     * @param userDetails
+     * @return
+     */
+    public String generateToken(UserDetails userDetails){
+        Map<String, Object> claims = new HashMap<>();
+        claims.put(CLAIM_KEY_USERNAME, userDetails.getUsername());
+        claims.put(CLAIM_KEY_CREATED, new Date());
+        return generateToken(claims);
+
+    }
+
+    /**
+     * 从token中获取用户名
+     * @param token
+     * @return
+     */
+    public String getUserNameFromToken(String token){
+        String username;
+        try {
+
+            Claims claims = getClaimsFromToken(token);
+            username = claims.getSubject();
+        } catch (Exception e){
+            username = null;
+        }
+        return  username;
+    }
+
+    /**
+     *  验证token 是否有效
+     * @param token
+     * @param userDetails
+     * @return
+     */
+    public boolean vaildateToken(String token, UserDetails userDetails){
+        String username = getUserNameFromToken(token);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    /**
+     *  判断token是否可用被刷新
+     * @param token
+     * @return
+     */
+    public boolean canRefresh(String token){
+        return !isTokenExpired(token);
+    }
+
+    /**
+     * 刷新token
+     * @param token
+     * @return
+     */
+    public String refreshToken(String token){
+        Claims claims = getClaimsFromToken(token);
+        claims.put(CLAIM_KEY_CREATED, new Date());
+        return generateToken(claims);
+    }
+
+    /**
+     * 判断token是否失效
+     * @param token
+     * @return
+     */
+    private boolean isTokenExpired(String token) {
+      Date expireDate = getExpirationDateFromToken(token);
+      return  expireDate.before(new Date());
+    }
+
+    /**
+     *  从token中获取荷载
+     * @param token
+     * @return
+     */
+    private Date getExpirationDateFromToken(String token) {
+        Claims claims = getClaimsFromToken(token);
+        return  claims.getExpiration();
+    }
+
+    /**
+     *  从token中获取荷载
+     * @param token
+     * @return
+     */
+    private Claims getClaimsFromToken(String token) {
+        Claims claims = null;
+        try {
+            claims = Jwts.parser()
+                    .setSigningKey(secret)
+                    .parseClaimsJwt(token)
+                    .getBody();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return  claims;
+    }
+
+    /**
+     * 根据荷载生成JWT TOKEN
+     * @param claims
+     * @return
+     */
+    private String generateToken(Map<String,Object> claims) {
+        return Jwts.builder().setClaims(claims)
+                .setExpiration(generateExpirationDate())
+                .signWith(SignatureAlgorithm.ES512,secret)
+                .compact();
+    }
+
+    /**
+     * 生成失效时间
+     * @return
+     */
+    private Date generateExpirationDate() {
+        return new Date(System.currentTimeMillis()+expiration*1000);
+    }
+}
---
+++ yeb/yeb-server/src/main/java/com/lemon/server/utils.java
@@ -0,0 +1,10 @@
+package com.lemon.server;
+
+/**
+ * @Date : 2021/4/8 0:39
+ * @author: CarlLing
+ * @Version 1.0
+ * @Desc : 
+ */
+public class utils {
+}
